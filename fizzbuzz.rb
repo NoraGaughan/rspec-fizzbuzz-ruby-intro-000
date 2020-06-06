84fd66c85ea182fb7ce8b9bfdb5a845bc4913c25
Nora Gaughan <noragaughan@gmail.com>
2020-06-06 21:19:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(int)
-  elsif int % 3 == 0 && int % 5 == 0 
-    "Fizzbuzz"
-  elsif int % 5 == 0 
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 
+  elsif int % 3 == 0
     "Fizz"
-  elsif 
-    "nil"
+  else nil
   end
 end
